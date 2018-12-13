090c4e4a1f6189a6d6121f555cd28d244d3d58b1
AntoCr <add8596.@gmail.com>
2018-12-14 00:04:38 +0100

    adding new methods

--- forms.js
+++ forms.js
@@ -1,2 +1,26 @@
-let elem = window.parent.document.getElementsByClassName("klynt-button")[0]; 
-console.log(elem);
+/*let elem = window.parent.document.getElementsByClassName("klynt-button")[0]; 
+console.log(elem);*/
+
+let button = "klynt-button";
+
+$(document).on('keydown', function(){
+
+	if(event.key == 'Enter'){
+		let nodeList = document.getElementsByTagName('input');
+		let count = 0;
+		for (let elem of nodeList){
+			if (elem.value == ""){
+				$(elem).popover('show');
+			}
+			else{
+				$(elem).popover('destroy');
+				count++;
+			}
+		}
+
+		if (count == nodeList.length){
+			window.parent.postMessage(button, '*')
+		}
+	}
+
+});
